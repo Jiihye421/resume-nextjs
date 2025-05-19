a264e22e7a0e5775e6a9a2da406daad75c4a649c
Jihye <bsy1240@naber.com>
2025-05-19 16:53:18 +0900

    자기소개 수정

--- payload/_global.ts
+++ payload/_global.ts
@@ -2,8 +2,8 @@ import favicon from '../asset/favicon.ico';
 import previewImage from '../asset/preview.jpg';
 import { IGlobal } from '../component/common/IGlobal';
 
-const title = 'RESUME-NEXTJS Sample Page';
-const description = 'This is a simple web resume sample.';
+const title = '김지혜의 이력서';
+const description = '김지혜의 포트폴리오 및 이력서입니다.';
 
 export const _global: IGlobal.Payload = {
   favicon,
@@ -19,14 +19,14 @@ export const _global: IGlobal.Payload = {
           url: previewImage,
           width: 800,
           height: 600,
-          alt: 'OpenGraph Image Sample.png',
+          alt: '김지혜의 이력서 미리보기',
         },
       ],
       type: 'profile',
       profile: {
-        firstName: 'Lorem',
-        lastName: 'ipsum',
-        username: 'lorem',
+        firstName: '지혜',
+        lastName: '김',
+        username: 'kjh',
         gender: 'female',
       },
     },
--- payload/introduce.ts
+++ payload/introduce.ts
@@ -4,7 +4,9 @@ import { lastestUpdatedAt } from '../package.json';
 const introduce: IIntroduce.Payload = {
   disable: false,
 
-  contents: ['배재대학교 컴퓨터공학과 4학년이고, 재학중입니다.'],
+  contents: [
+    '안녕하세요. 배재대학교 컴퓨터공학과 4학년이고, 재학중입니다. 실행력과 협업을 바탕으로, 사용자 중심의 웹 개발과 시스템 설계에 관심을 가지고 성장 중입니다.',
+  ],
   sign: 'Jihye Kim',
   // sign: packageJson.author.name,
   // latestUpdated: '2019-07-24',
